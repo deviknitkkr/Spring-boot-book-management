cda6438a726b29ea8e81c9a1d06358cb3840bbc9
deviknitkkr <deviknitkkr3@gmail.com>
2021-04-07 11:05:35 +0530

    initialized project

---
+++ src/main/java/com/devik/springbootbookmanagement/SpringBootBookManagementApplication.java
@@ -0,0 +1,13 @@
+package com.devik.springbootbookmanagement;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootBookManagementApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootBookManagementApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/devik/springbootbookmanagement/SpringBootBookManagementApplicationTests.java
@@ -0,0 +1,13 @@
+package com.devik.springbootbookmanagement;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootBookManagementApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
