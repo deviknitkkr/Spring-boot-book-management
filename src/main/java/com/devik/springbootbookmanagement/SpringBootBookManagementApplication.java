4320a8145189ff184dd9e32c79ae4bef8aff797b
deviknitkkr <deviknitkkr3@gmail.com>
2021-04-07 11:43:03 +0530

    first commit

--- src/main/java/com/devik/springbootbookmanagement/SpringBootBookManagementApplication.java
+++ src/main/java/com/devik/springbootbookmanagement/SpringBootBookManagementApplication.java
@@ -3,11 +3,13 @@ package com.devik.springbootbookmanagement;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+
 @SpringBootApplication
 public class SpringBootBookManagementApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(SpringBootBookManagementApplication.class, args);
+		System.out.println("Server has started...");
 	}
 
 }
